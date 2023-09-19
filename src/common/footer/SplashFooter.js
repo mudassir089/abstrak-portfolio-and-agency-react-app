267ca872de0658a6e87155c6f1c78d09cf24dc02
mudassir089 <muhammadmudassir0900@gmail.com>
2023-09-19 16:46:20 +0500

    abstrak portfolio and agency project

---
+++ src/common/footer/SplashFooter.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaDribbble } from "react-icons/fa";
+
+const SplashFooter = () => {
+  return (
+    <footer className="footer-area splash-footer">
+      <div className="container">
+        <div className="footer-bottom">
+          <div className="row align-items-center">
+            <div className="col-lg-5">
+              <div className="footer-copyright">
+                <span className="copyright-text">
+                  © {new Date().getFullYear()}. All rights reserved by{" "}
+                  <a href="#">Abstrak</a>.
+                </span>
+              </div>
+            </div>
+            <div className="col-lg-2">
+              <ul className="footer-social list-unstyled">
+                <li>
+                  <a href="https://www.facebook.com">
+                    <FaFacebookF />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://dribbble.com">
+                    <FaDribbble />
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-5">
+              <div className="footer-bottom-link">
+                <ul className="list-unstyled">
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/privacy-policy"}>
+                      Privacy Policy
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default SplashFooter;
